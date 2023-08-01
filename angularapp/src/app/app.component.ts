dde04f02687b4980c709c47687a2318f1e4d8b1b
Iamneo <techadmin@iamneo.ai>
2023-08-01 14:30:08 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';   
+  title = 'angularapp';
   opt1:number;
   opt2:number;
   amt:number;
